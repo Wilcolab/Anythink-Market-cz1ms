7554620b24266e59cba21250b1019d07507e58df
Dan Behrman <danb@primematterlabs.com>
2022-08-29 18:06:12 -0400

    filter by title if present in params

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.includes(:tags)
-
+    @items = @items.where('lower(title) LIKE ?', "%#{params[:title].downcase}%") if params[:title].present?
     @items = @items.tagged_with(params[:tag]) if params[:tag].present?
     @items = @items.sellered_by(params[:seller]) if params[:seller].present?
     @items = @items.favorited_by(params[:favorited]) if params[:favorited].present?
